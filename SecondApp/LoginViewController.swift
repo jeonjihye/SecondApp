1c529e540d8626d8318402b55437e39bf22d69f1
Jihye Jeon <jeonjh9535@naver.com>
2021-08-24 11:11:48 +0900

    로그인화면에서 아이디 정보넘기기

--- SecondApp/LoginViewController.swift
+++ SecondApp/LoginViewController.swift
@@ -7,10 +7,47 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+protocol LoginReusltProtocol {
+    func setId(id : String)
+}
 
+class LoginViewController: UIViewController {
+    
+    var loginInfo : Dictionary<String,String> = [:]
+    // 로그인화면을 오픈한 ViewContorller 인스턴스 할당하는 변수
+    var resultViewController : LoginReusltProtocol?
+    
+    
+    @IBOutlet weak var IdTextFiled: UITextField!
+    @IBOutlet weak var passwardTextField: UITextField!
+    
+    
+    @IBAction func cancel(_ sender: Any) {
+        // 아무것도 해주지 말기
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        // id&pw 값 전달
+        var id = IdTextFiled.text
+        var pw = passwardTextField.text
+        
+        guard let idValue = IdTextFiled.text,
+              let pwValue = passwardTextField.text else {
+            return
+        }
+        
+        self.resultViewController?.setId(id : idValue)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var cancel: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 데이터전달
+        
+        
 
         // Do any additional setup after loading the view.
     }
--- SecondApp/ViewController.swift
+++ SecondApp/ViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    // 코드로 모달 띄우기
-    @IBAction func openLogin(_ sender: Any) {
-        var loginVC = self.storyboard?.instantiateViewController(identifier: "login")
-        
+class ViewController: UIViewController, LoginReusltProtocol  {
+    func setId(id: String) {
+        self.idLabel.text = id
     }
+
+    // 아이디를 띄울 라벨
+    @IBOutlet weak var idLabel: UILabel!
+    //var idText : String?
     
-    
+     
     @IBOutlet weak var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +41,37 @@ class ViewController: UIViewController {
 
         // Use data from the view controller which initiated the unwind segue
     }
+    
+    // 코드로 모달 띄우기
+    @IBAction func openLogin(_ sender: Any) {
+        /*
+        // if 문 > 코드 안에서만 사용가능
+        if let loginVC1 = self.storyboard?.instantiateViewController(identifier: "login"){
+            
+            loginVC1
+        }
+        */
+        // Guard 문 > 코드 밖에서만 사용가능
+        guard let loginVC = self.storyboard?.instantiateViewController(identifier: "login") as? LoginViewController else{
+            return
+        }
+        // 1) 모달형태로 띄우기
+        loginVC.resultViewController = self
+        self.present(loginVC, animated: true ,completion: nil)
+        
+        // 2) Push로 띄우기 : 네비게이션 컨트롤러에게 요청
+        //self.navigationController?.pushViewController(loginVC, animated: true)
 
+        
+    }
+    // 아이디 라벨에 아이디 출력
+    // 모달은 viewWillAppear 호출 안됨
+    /*
+    override func viewWillAppear(_ animated: Bool) {
+        idLabel.text = idText
+    }
+    */
+    
 
 }
 
